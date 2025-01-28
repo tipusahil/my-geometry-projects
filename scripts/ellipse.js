bf420b41c91b9edb1c19aa87b8573f8ed6e578e8
tipusahil <tipusahil.ctg@gmail.com>
2025-01-29 02:40:26 +0600

    ellipse done

--- scripts/ellipse.js
+++ scripts/ellipse.js
@@ -1,3 +1,8 @@
 function ellipse_calculateBTN(){
-    
+    const a = getinputTagByID('ellipse_a_input');
+    const b = getinputTagByID('ellipse_b_input');
+
+    const area = Math.PI * a * b ;
+
+    setInputValueInTAG('ellipse_output_set_tag' , area);
 }
