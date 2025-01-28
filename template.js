238bd75cfee78580b3a346f3e7ae0169357ba630
tipusahil <tipusahil.ctg@gmail.com>
2025-01-29 02:45:52 +0600

    templete js file created

--- template.js
+++ template.js
@@ -1,18 +1,3 @@
-function ellipse_calculateBTN(){
-    const a = getinputTagByID('ellipse_a_input');
-    const b = getinputTagByID('ellipse_b_input');
-
-    const area = Math.PI * a * b ;
-
-    setInputValueInTAG('ellipse_output_set_tag' , area);
-}
-
-// niser shorthand template gulo pentagon name js file e lika hoise just oi file er function er name call kore   ellipse er idname pass korar maddome ellipse ar sob kaj hoye gelo, notun kore input er jonno kinba output tag e set korar jonno notun kore kuno function likte hoini.
-
-
-
-
-/*
 //1. input er value pawar jonno shorthand e function just idta pass kore dile hobe, jei id dibo oi id er value return kore dibe.
 function getinputTagByID(inputTAG){
     const input_tags = document.getElementById(inputTAG);
@@ -34,6 +19,3 @@ function setInputValueInTAG(set_id_tag, calculate){
     return set_value_tag;
 }
 
-
-
-*/
